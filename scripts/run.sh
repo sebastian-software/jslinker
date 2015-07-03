79e301438a28b19fd1b69c50ee9661ecc854829b
Sebastian Fastner <s.fastner@sebastian-software.de>
2015-07-03 18:58:32 +0200

    First running version
    - no error code, so only good case scenario
    - uses always macos iojs

---
+++ scripts/run.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+OUTPUT="$(pwd)/.jslinker"
+
+die() {
+  echo "Error: $*"
+  exit 1
+}
+
+if [ -z `which tar` ]; then
+  die 'tar not found'
+fi
+
+if [ -f "$OUTPUT" ]; then
+  die 'output path must be directory'
+fi
+
+[[ ! -d "$OUTPUT" ]] && mkdir "$OUTPUT"
+
+SKIPLINES=`awk '/^__END__/ { print NR + 1; exit 0; }' $0`
+
+tail -n +$SKIPLINES $0 | tar -xz -C "$OUTPUT"
+if [ $? != 0 ]; then
+  die "cannot extract the files"
+fi
